638e67666dec23e73e9e099d25389ac6533539d0
fahdikrie <fahdiaf@gmail.com>
2020-10-31 05:49:49 +0700

    feat: change mobile layout for social icons

--- components/Layout/NavigationBar/index.jsx
+++ components/Layout/NavigationBar/index.jsx
@@ -4,13 +4,15 @@ import GithubIcon from 'public/assets/github1.svg'
 import LinkedInIcon from 'public/assets/linkedin1.svg'
 
 import {
-  NavWrapper
+  NavWrapper,
+  NavSocialsWrapper,
+  NavItemsWrapper,
 } from './index.style'
 
 const NavigationBar = (props) => {
   return (
     <NavWrapper>
-      <div className="nav-socials">
+      <NavSocialsWrapper className="nav-socials">
         <img
           className="nav-socials__nav-social github"
           src={GithubIcon}
@@ -21,18 +23,18 @@ const NavigationBar = (props) => {
           src={LinkedInIcon}
           alt="linkedin"
         />
-      </div>
+      </NavSocialsWrapper>
 
-      <ul className="nav-items">
+      <NavItemsWrapper className="nav-items">
         {(props.currentPage !== "index")
-          ? <li className="nav-items__nav-item">home</li>
+          ? <li className="nav-items__nav-item"><a href="/">home</a></li>
           : ""}
-        <li className="nav-items__nav-item">resume</li>
+        <li className="nav-items__nav-item"><a href="/">resume</a></li>
         {(props.currentPage !== "activities")
-          ? <li className="nav-items__nav-item">activities</li>
+          ? <li className="nav-items__nav-item"><a href="/activities">activities</a></li>
           : ""}
-        <li className="nav-items__nav-item">journal</li>
-      </ul>
+        <li className="nav-items__nav-item"><a href="/">journal</a></li>
+      </NavItemsWrapper>
     </NavWrapper>
   )
 }
--- components/Layout/NavigationBar/index.style.js
+++ components/Layout/NavigationBar/index.style.js
@@ -1,21 +1,9 @@
 import styled from 'styled-components'
 import { fallbackFonts } from 'public/fonts/fonts'
 
-export const NavWrapper = styled.div`
-  position: absolute;
-  z-index: 1;
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
 
-  margin: 0;
-  width: calc(100% - 6%);
-  padding: 15px 3%;
-
-  color: white;
-  font-family: 'San Francisco Pro Bold', ${fallbackFonts};
-
-  & div.nav-socials {
+export const NavSocialsWrapper = styled.div`
+  & {
     position: fixed;
     bottom: 40px;
     left: 20px;
@@ -42,12 +30,14 @@ export const NavWrapper = styled.div`
       }
     }
   }
+`
 
-
-  & ul.nav-items {
+export const NavItemsWrapper = styled.ul`
+   & {
     display: flex;
     list-style-type: none;
     font-size: 1.1rem;
+    padding: 0;
 
     & li.nav-items__nav-item {
       margin-left: 50px;
@@ -62,12 +52,27 @@ export const NavWrapper = styled.div`
       }
     }
   }
+`
+
+export const NavWrapper = styled.div`
+  position: absolute;
+  z-index: 1;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+
+  margin: 0;
+  width: calc(100% - 6%);
+  padding: 15px 3%;
+
+  color: white;
+  font-family: 'San Francisco Pro Bold', ${fallbackFonts};
 
   @media (min-width: 1660px) {
     width: calc(100% - 6%);
     padding: 35px 3%;
 
-    & div.nav-socials {
+    & ${NavSocialsWrapper} {
       height: 120px;
       bottom: 60px;
       left: 30px;
@@ -81,7 +86,7 @@ export const NavWrapper = styled.div`
       }
     }
 
-    & ul.nav-items {
+    & ${NavItemsWrapper} {
       font-size: 1.65rem;
 
       & li.nav-items__nav-item {
@@ -90,10 +95,40 @@ export const NavWrapper = styled.div`
     }
   }
 
+  @media (max-width: 768px) {
+    padding: 10px 3%;
+    justify-content: space-between;
+
+    & ${NavSocialsWrapper} {
+      position: static;
+      flex-direction: row;
+      justify-content: space-between;
+
+      height: fit-content;
+      width: 57.5px;
+
+      & img {
+        width: calc(1rem * 1.5);
+
+        &.nav-socials__nav-social.github {
+          width: calc(1rem * 1.5);
+        }
+      }
+    }
+
+    & ${NavItemsWrapper} {
+      font-size: 1rem;
+
+      & li.nav-items__nav-item {
+        margin-left: 25px;
+      }
+    }
+  }
+
   @media (max-width: 525px) {
     padding: 10px 3%;
 
-    & ul.nav-items {
+    & ${NavItemsWrapper} {
       font-size: 1rem;
 
       & li.nav-items__nav-item {
@@ -103,7 +138,7 @@ export const NavWrapper = styled.div`
   }
 
   @media (max-width: 376px) {
-    & ul.nav-items {
+    & ${NavItemsWrapper} {
       font-size: 0.9rem;
 
       & li.nav-items__nav-item {
@@ -113,14 +148,8 @@ export const NavWrapper = styled.div`
   }
 
   @media (max-width: 330px) {
-    & ul.nav-items {
+    & ${NavItemsWrapper} {
       font-size: 0.75rem;
     }
   }
-`
-
-export const MobileNavWrapper = styled(NavWrapper)`
-
-
-
 `
