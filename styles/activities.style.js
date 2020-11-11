e81aea058e901e3e30782c42919e4c539125837a
fahdikrie <fahdiaf@gmail.com>
2020-11-11 23:41:03 +0700

    fix: change padding for activites on tablet & mobile

--- styles/activities.style.js
+++ styles/activities.style.js
@@ -1,8 +1,13 @@
 import styled from 'styled-components'
 
 export const ActivitiesWrapper = styled.div`
-  width: calc(100% - 14%);
-  padding: 100px 7% 0;
+  width: calc(100% - 16%);
+  padding: 100px 8% 0;
+
+  @media (max-width: 768px) {
+    width: calc(100% - 10%);
+    padding: 100px 5% 0;
+  }
 `
 export const PosterCardsContainer = styled.div`
   width: 100%;
