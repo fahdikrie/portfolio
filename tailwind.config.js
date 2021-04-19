5159b14ca5d69fd094f3afa7d342329e703d2787
fahdikrie <fahdiaf@gmail.com>
2021-04-19 07:28:37 +0700

    build: delete package-lock & add twcss purge config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
-  purge: [],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontSize: {
