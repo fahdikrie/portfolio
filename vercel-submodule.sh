2d7668ffbdaf74c7871252bd2a6be1b03e487dda
Fahdii Ajmalal Fikrie <fahdiaf@gmail.com>
2021-05-15 19:10:10 +0700

    build: modify script

--- vercel-submodule.sh
+++ vercel-submodule.sh
@@ -36,6 +36,7 @@ git checkout $VERCEL_GIT_COMMIT_SHA
 git config --file=.gitmodules "submodule.$SUBMODULE_REF.url" https://$GITHUB_ACCESS_TOKEN@$SUBMODULE_REPO
 
 # checkout the submodule
+git pull --recurse-submodules
 git submodule sync
 git submodule update --init --recursive --remote
 
