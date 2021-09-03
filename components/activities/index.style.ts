aeb4d96a14f01bd42fb3dc2c035dfb02fbab939f
Fahdii Ajmalal Fikrie <fahdiaf@gmail.com>
2021-09-03 12:24:20 +0700

    style: run prettier

--- components/activities/index.style.ts
+++ components/activities/index.style.ts
@@ -1,16 +1,16 @@
-import tw from 'twin.macro'
+import tw from 'twin.macro';
 
 export const Activities = tw.div`
   relative w-full
   px-24 md:px-64 lg:px-80 xl:px-100
   pt-80 md:pt-100
-`
+`;
 
 export const RecentsWrapper = tw.div`
   grid
   gap-20 lg:gap-48
   grid-cols-1 md:grid-cols-2
-`
+`;
 
 export const FavoritesWrapper = tw.div`
   grid
@@ -18,4 +18,4 @@ export const FavoritesWrapper = tw.div`
   grid-cols-1 md:grid-cols-2
   mt-36 md:mt-48
   mb-64
-`
+`;
--- pages/api/favorite-movies.ts
+++ pages/api/favorite-movies.ts
@@ -1,6 +1,6 @@
-import { FavoriteMovies } from 'data/favorite-movies-data'
+import { FavoriteMovies } from 'data/favorite-movies-data';
 
 export default function handler(req, res) {
   // Get data from your database
-  res.status(200).json(JSON.stringify(FavoriteMovies))
+  res.status(200).json(JSON.stringify(FavoriteMovies));
 }
