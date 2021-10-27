48adc77c4ac7d8b3643aef4043e65638965911f0
Fahdii Ajmalal Fikrie <fahdiaf@gmail.com>
2021-10-28 01:51:07 +0700

    build(pvt-submodule): modify script

--- submodule.sh
+++ submodule.sh
@@ -1,5 +1,5 @@
 # github submodule repo address without https:// prefix
-SUBMODULE_GITHUB=github.com/beeinger/vercel-private-submodule
+SUBMODULE_GITHUB=github.com/fahdikrie/content
 
 # .gitmodules submodule path
 SUBMODULE_PATH=library
