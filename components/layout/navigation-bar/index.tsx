296ca30984edf6c36210e6365951766176665a6d
Fahdii Ajmalal Fikrie <fahdiaf@gmail.com>
2021-12-27 02:36:50 +0700

    fix(navbar): fix fout due to unloaded resolvedTheme

--- components/layout/navigation-bar/index.tsx
+++ components/layout/navigation-bar/index.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 import LinkedInIconLight from 'public/assets/linkedin-light.svg';
 import GithubIconLight from 'public/assets/github-light.svg';
@@ -14,8 +15,18 @@ interface NavigationBarProps {
 
 const NavigationBar = ({ currentPage }: NavigationBarProps) => {
   const { resolvedTheme, setTheme } = useTheme();
-  const GithubIcon = resolvedTheme === 'light' ? GithubIconLight : GithubIconDark;
-  const LinkedInIcon = resolvedTheme === 'light' ? LinkedInIconLight : LinkedInIconDark;
+  const [githubIcon, setGithubIcon] = useState();
+  const [linkedInIcon, setLinkedInIcon] = useState();
+
+  useEffect(() => {
+    if (resolvedTheme) {
+      const GithubIcon = resolvedTheme === 'light' ? GithubIconLight : GithubIconDark;
+      const LinkedInIcon = resolvedTheme === 'light' ? LinkedInIconLight : LinkedInIconDark;
+
+      setGithubIcon(GithubIcon);
+      setLinkedInIcon(LinkedInIcon);
+    }
+  }, [resolvedTheme])
 
   const onSetTheme = () => {
     if (resolvedTheme === 'light') {
@@ -29,24 +40,28 @@ const NavigationBar = ({ currentPage }: NavigationBarProps) => {
     <>
       <S.Wrapper>
         <S.Socials>
-          <S.ThemeButton
-            onClick={() => onSetTheme()}
-            isLightTheme={resolvedTheme === 'light'}
-          />
-          <a
-            href="https://github.com/fahdikrie"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <S.Social src={GithubIcon} alt="github" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/fahdikrie/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <S.Social src={LinkedInIcon} alt="linkedin" />
-          </a>
+          {resolvedTheme && (
+            <>
+              <S.ThemeButton
+                onClick={() => onSetTheme()}
+                isLightTheme={resolvedTheme === 'light'}
+              />
+              <a
+                href="https://github.com/fahdikrie"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <S.Social src={githubIcon} alt="github" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/fahdikrie/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <S.Social src={linkedInIcon} alt="linkedin" />
+              </a>
+            </>
+          )}
         </S.Socials>
         <S.Items>
           <S.Item isActive={currentPage == '/' ? true : false}>
