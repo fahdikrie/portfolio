726d9a89844612a54eb2ddc0045b01b09f7580bf
Fahdii Ajmalal Fikrie <fahdii.ajmalal91@ui.ac.id>
2022-03-01 03:02:38 +0700

    feat(journal): add data fetching logic

--- components/journal/posts/index.tsx
+++ components/journal/posts/index.tsx
@@ -1,24 +1,3 @@
-import { getAllPosts } from 'libs/notion'
-import BLOG from 'blog.config'
-
-export async function getStaticProps () {
-  const posts = await getAllPosts({ includePages: false })
-  const postsToShow = posts.slice(0, BLOG.postsPerPage)
-  const totalPosts = posts.length
-  const showNext = totalPosts > BLOG.postsPerPage
-
-  console.log(posts)
-
-  return {
-    props: {
-      page: 1,
-      postsToShow,
-      showNext
-    },
-    revalidate: 1
-  }
-}
-
 const blog = () => {
   return (
     <></>
--- pages/journal/index.tsx
+++ pages/journal/index.tsx
@@ -1,14 +1,40 @@
 import { default as Index } from 'components/journal';
 import Layout from 'components/layout';
 
-const Journal = ({ router }): JSX.Element => (
-  <Layout
-    description="An internet-web journal of Fahdii Ajmalal Fikrie."
-    currentPage={router.pathname}
-    pageTitle="journal"
-  >
-    <Index />
-  </Layout>
-);
+import { getAllPosts } from 'libs/notion'
+import BLOG from 'blog.config'
+import { useEffect } from 'react';
+
+export async function getStaticProps () {
+  const posts = await getAllPosts({ includePages: false })
+  const postsToShow = posts.slice(0, BLOG.postsPerPage)
+  const totalPosts = posts.length
+  const showNext = totalPosts > BLOG.postsPerPage
+
+  return {
+    props: {
+      page: 1,
+      postsToShow: postsToShow,
+      showNext: showNext
+    },
+    revalidate: 1
+  }
+}
+
+const Journal = ({ router, page, postsToShow, showNext }): JSX.Element => {
+  useEffect(() => {
+    console.log(postsToShow)
+  }, [postsToShow])
+
+  return (
+    <Layout
+      description="An internet-web journal of Fahdii Ajmalal Fikrie."
+      currentPage={router.pathname}
+      pageTitle="journal"
+    >
+      <Index />
+    </Layout>
+  );
+};
 
 export default Journal;
