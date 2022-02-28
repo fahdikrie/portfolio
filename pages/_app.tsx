fc0049db9a6886bb1e8070f22bb19ebe778228e0
Fahdii Ajmalal Fikrie <fahdii.ajmalal91@ui.ac.id>
2022-03-01 01:03:46 +0700

    chore: run prettier & move journal.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,17 +9,13 @@ import FontSerif from 'public/fonts/font-serif';
 import 'styles/global.css';
 
 // core styles shared by all of react-notion-x (required)
-import 'react-notion-x/src/styles.css'
-
+import 'react-notion-x/src/styles.css';
 // used for code syntax highlighting (optional)
-import 'prismjs/themes/prism-tomorrow.css'
-
+import 'prismjs/themes/prism-tomorrow.css';
 // used for collection views (optional)
-import 'rc-dropdown/assets/index.css'
-
+import 'rc-dropdown/assets/index.css';
 // used for rendering equations (optional)
-import 'katex/dist/katex.min.css'
-
+import 'katex/dist/katex.min.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
