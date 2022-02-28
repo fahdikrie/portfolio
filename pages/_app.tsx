2854b190f11999d2e45130b81d68d267c4b11ffa
Fahdii Ajmalal Fikrie <fahdii.ajmalal91@ui.ac.id>
2022-03-01 01:02:13 +0700

    feat(journal): add styling to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,19 @@ import FontSans from 'public/fonts/font-sans';
 import FontSerif from 'public/fonts/font-serif';
 import 'styles/global.css';
 
+// core styles shared by all of react-notion-x (required)
+import 'react-notion-x/src/styles.css'
+
+// used for code syntax highlighting (optional)
+import 'prismjs/themes/prism-tomorrow.css'
+
+// used for collection views (optional)
+import 'rc-dropdown/assets/index.css'
+
+// used for rendering equations (optional)
+import 'katex/dist/katex.min.css'
+
+
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
