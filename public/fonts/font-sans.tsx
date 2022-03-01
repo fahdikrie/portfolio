651bf8e088ef13a34e74e5a13d6cfd0ec14cda32
Fahdii Ajmalal Fikrie <fahdii.ajmalal91@ui.ac.id>
2022-03-01 11:16:31 +0700

    chore: change font files to tsx

--- public/fonts/font-sans.tsx
+++ public/fonts/font-sans.tsx
@@ -1,8 +1,10 @@
+// @ts-nocheck
 import Head from 'next/head';
 
-const FontSerif = () => (
+const FontSans = () => (
   <>
     <Head>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js" />
       <link
         rel="preconnect"
         href="https://fonts.gstatic.com"
@@ -11,22 +13,22 @@ const FontSerif = () => (
       <link
         rel="stylesheet preload"
         as="style"
-        href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;0,900;1,400;1,700&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@500;700;900&display=swap"
       />
       <link
         rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;0,900;1,400;1,700&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@500;700;900&display=swap"
         media="print"
         onLoad="this.media='all'"
       />
       <noscript>
         <link
           rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;0,900;1,400;1,700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@500;700;900&display=swap"
         />
       </noscript>
     </Head>
   </>
 );
 
-export default FontSerif;
+export default FontSans;
