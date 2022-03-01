ef3a4ad73a8a0a63dce439fc202857c6da385171
Fahdii Ajmalal Fikrie <fahdii.ajmalal91@ui.ac.id>
2022-03-01 17:22:15 +0700

    chore: add husky & lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,8 @@
-const lintStagedConfig = {
+module.exports = {
   '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
   '**/*.(ts|tsx|js)': (filenames) => [
     `yarn eslint --fix ${filenames.join(' ')}`,
     `yarn prettier --write ${filenames.join(' ')}`,
   ],
-  '**/*.(md|json)': (filenames) =>
-    `yarn prettier --write ${filenames.join(' ')}`,
+  '**/*.(md|json)': (filenames) => `yarn prettier --write ${filenames.join(' ')}`,
 };
-
-export default lintStagedConfig;
