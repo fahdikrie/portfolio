cc06518566bc7ab2a0ad5b878d4be62f6562c696
Fahdii Ajmalal Fikrie <fahdii.ajmalal91@ui.ac.id>
2022-04-05 12:09:01 +0700

    feat(seo): modify index page desc again

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Layout from 'components/layout';
 import Home from 'components/containers/Home';
 
-const DESCRIPTION = `A personal webpage of Fahdii Ajmalal Fikrie (goes by the name Fahdii, Badi, Fahdikrie, or Dikrie). Fahdii is a 21 years old CS student at the University of Indonesia.`;
+const DESCRIPTION = `A personal webpage of Fahdii Ajmalal Fikrie (goes by the name Fahdii, Badi, Fahdikrie, or Dikrie). Fahdii is a 21 y/o CS student at University of Indonesia.`;
 
 const HomePage = ({ router }): JSX.Element => (
   <Layout
