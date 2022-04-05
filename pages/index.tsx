fc302ea76662f3431ad8382b2240ad73abc393a8
Fahdii Ajmalal Fikrie <fahdii.ajmalal91@ui.ac.id>
2022-04-05 11:43:55 +0700

    feat(seo): change index page description

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import Layout from 'components/layout';
 import Home from 'components/containers/Home';
 
-const DESCRIPTION = `Fahdii Ajmalal Fikrie (goes by the name Fahdii, Badi, Fahdikrie, or Dikrie both IRL and on the internet-web) is a 21 years old sophomore student currently majoring CS at the University of Indonesia.`;
+const DESCRIPTION = `(goes by the name Fahdii, Badi, Fahdikrie, or Dikrie both IRL and on the internet-web). Fahdii is a 21 years old sophomore student currently majoring CS at the University of Indonesia.`;
 
 const HomePage = ({ router }): JSX.Element => (
   <Layout
-    pageDescription={`A web portfolio of Fahdii Ajmalal Fikrie. ${DESCRIPTION}`}
+    pageDescription={`A web portfolio of Fahdii Ajmalal Fikrie ${DESCRIPTION}`}
     currentPage={router.pathname}
     pageTitle="Home"
   >
