e7b293e95ced02b14a5578b90fe9f76d157e848e
Fahdii Ajmalal Fikrie <fahdii.ajmalal91@ui.ac.id>
2022-08-03 20:44:39 +0700

    chore: add .env.template

--- libs/notion/index.ts
+++ libs/notion/index.ts
@@ -1,8 +1,8 @@
 import { NotionAPI } from 'notion-client';
 
-export const NOTION_BLOG_PAGE_ID = '30cb0f29326440afb9f528e125fe8ca5';
+export const NOTION_BLOG_PAGE_ID = process.env.NOTION_BLOG_PAGE_ID;
 
-export const NOTION_JOURNAL_PAGE_ID = '7d689ff0d95d42f2bc4f01fcf0c9b7b9';
+export const NOTION_JOURNAL_PAGE_ID = process.env.NOTION_JOURNAL_PAGE_ID;
 
 const notion = new NotionAPI();
 export default notion;
